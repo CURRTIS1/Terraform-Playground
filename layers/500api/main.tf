b79801e4f07a19bc5e83a8726dd941fe21edf4d9
Curtis Wallond <curtis.wallond@rackspace.co.uk>
2022-06-28 12:43:31 +0100

    changed api name

--- layers/500api/main.tf
+++ layers/500api/main.tf
@@ -181,7 +181,7 @@ resource "aws_api_gateway_rest_api" "TestAPI" {
 resource "aws_api_gateway_resource" "MyAPIResource" {
   rest_api_id = aws_api_gateway_rest_api.TestAPI.id
   parent_id   = aws_api_gateway_rest_api.TestAPI.root_resource_id
-  path_part   = "testing"
+  path_part   = "get-test-lambda"
 }
 
 
