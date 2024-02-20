20176f3948b348c738c9abcfe3176fa83f103ac4
currtis1 <curtiswallond@gmail.com>
2024-02-20 17:54:49 +0000

    adding

--- layers/000base/secret.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  description = "The AWS Access Key"
-  default     = "AKIA3YVYKF4AVII2M7HV"
-}
-
-variable "aws_secret_key" {
-  description = "The AWS Secret Key"
-  default     = "KY5rJ0flh+4Akc5KRNGabpwBGxozjtVYdj5g4C0F"
-}
